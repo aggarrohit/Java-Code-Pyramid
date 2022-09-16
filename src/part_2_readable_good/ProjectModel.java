cee73b5b6154a4df9f8b7957db4a7f0d08659307
Eduardo <el_alienx@hotmail.com>
2022-09-16 15:06:10 +0200

    changed files

--- src/part_2_readable_good/ProjectModel.java
+++ src/part_2_readable_good/ProjectModel.java
@@ -2,22 +2,20 @@ package part_2_readable_good;
 
 import part_2_readable_good.data.CurrencyPool;
 import part_2_readable_good.data.InventoryPool;
-import part_2_readable_good.utilities.CurrencyConverter;
 import part_2_readable_good.utilities.CurrencyItem;
 import part_2_readable_good.utilities.InventoryItem;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 public class ProjectModel {
     // Properties
     private final List<CurrencyItem> currencies = CurrencyPool.getCurrencies();
+    private final List<InventoryItem> inventory = InventoryPool.getInventory();
     private CurrencyItem selectedCurrency = currencies.get(0);
-    private List<List<String>> inventory = parseInventory();
 
     // Getters
-    public List<List<String>> getInventory() {
+    public List<InventoryItem> getInventory() {
         return inventory;
     }
 
@@ -29,27 +27,8 @@ public class ProjectModel {
         return selectedCurrency;
     }
 
-    // Public
-    public void handleOption(int selectedOption) throws NoSuchElementException {
+    // Setter
+    public void setSelectedCurrency(int selectedOption) throws NoSuchElementException {
         selectedCurrency = CurrencyPool.getItemById(selectedOption);
-        inventory = parseInventory();
-    }
-
-    // Private
-    private List<List<String>> parseInventory() {
-        List<InventoryItem> pool = InventoryPool.getInventory();
-        List<List<String>> result = new ArrayList<>();
-
-        for (InventoryItem item: pool) {
-            String name = item.name();
-            String image = item.image();
-            String description = item.description();
-            String price = CurrencyConverter.convert(item.price(), selectedCurrency.id());
-            List<String> data = List.of(name, image, description, price);
-
-            result.add(data);
-        }
-
-        return result;
     }
 }
--- src/part_2_readable_good/ProjectView.java
+++ src/part_2_readable_good/ProjectView.java
@@ -1,44 +1,78 @@
 package part_2_readable_good;
 
+import part_2_readable_good.data.CurrencyConverter;
 import part_2_readable_good.utilities.CurrencyItem;
-import part_2_readable_good.utilities.CurrencySelector;
+import part_2_readable_good.utilities.DropDownMenu;
+import part_2_readable_good.utilities.InventoryItem;
 import part_2_readable_good.utilities.Table;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProjectView {
     // Constructor
-    public ProjectView(List<CurrencyItem> currencies, CurrencyItem selectedCurrency, List<List<String>> inventory) {
-        showMenu(currencies, selectedCurrency, inventory);
+    public ProjectView(List<CurrencyItem> currencies, CurrencyItem selectedCurrency, List<InventoryItem> inventory) {
+        showData(currencies, selectedCurrency, inventory);
     }
 
     // Public
-    public void showMenu(List<CurrencyItem> currencies, CurrencyItem selectedCurrency, List<List<String>> inventory) {
-        showCurrencyName(selectedCurrency);
-        showTable(inventory);
-        showPrompt(currencies);
-    }
-
     public void showError() {
         System.out.println("⚠️ Invalid option");
     }
 
-    public void showPrompt(List<CurrencyItem> currencies) {
-        CurrencySelector currencySelector = new CurrencySelector(currencies);
+    public void showPrompt() {
+        System.out.print("Choose an option and then press enter: ");
+    }
 
-        currencySelector.showData();
+    public void showData(List<CurrencyItem> currencies, CurrencyItem selectedCurrency, List<InventoryItem> inventory) {
+        showCurrencyName(currencies, selectedCurrency);
+        showTable(inventory, selectedCurrency);
+        showPrompt();
     }
 
     // Private
-    private void showCurrencyName(CurrencyItem selectedCurrency) {
+    private void showCurrencyName(List<CurrencyItem> currencies, CurrencyItem selectedCurrency) {
+        List<String> currencyNames = parsedCurrencyNames(currencies);
+        DropDownMenu dropDownMenu = new DropDownMenu("Available currencies", currencyNames);
+
         System.out.println("Selected currency: " + selectedCurrency.name());
+        dropDownMenu.showData();
     }
 
-    private void showTable(List<List<String>> inventory) {
-        List<Integer> columnWidths = List.of(15, 5, 50, 15);
+    private void showTable(List<InventoryItem> inventory, CurrencyItem selectedCurrency) {
+        List<Integer> columnWidths = List.of(15, 5, 50, 10);
         List<String> headers = List.of("Product", "Image", "Description", "Price");
-        Table table = new Table(headers,inventory, columnWidths);
+        List<List<String>> body = parseInventory(inventory, selectedCurrency);
+        Table table = new Table(columnWidths, headers, body);
 
         table.showData();
+    }
+
+    private List<List<String>> parseInventory(List<InventoryItem> inventory, CurrencyItem selectedCurrency) {
+        List<List<String>> result = new ArrayList<>();
+
+        for (InventoryItem item: inventory) {
+            String name = item.name();
+            String image = item.image();
+            String description = item.description();
+            String price = CurrencyConverter.convert(item.price(), selectedCurrency.id());
+            List<String> data = List.of(name, image, description, price);
+
+            result.add(data);
+        }
+
+        return result;
+    }
+
+    private List<String> parsedCurrencyNames(List<CurrencyItem> currencies) {
+        List<String> result = new ArrayList<>();
+
+        for (CurrencyItem item: currencies) {
+            String name = item.name();
+
+            result.add(name);
+        }
+
+        return result;
     }
 }
