da4e2ae31c4de11f23b799f179edfb2266e02958
Eduardo <el_alienx@hotmail.com>
2023-06-05 11:32:08 +0200

    Reverted the first exercise changes done live in class during the first boot camp.

--- src/part_1_readable_bad/Item.java
+++
@@ -1,4 +0,0 @@
-package part_1_readable_bad;
-
-record Item(int id, String product, String image, String description, int price, String currency) {
-}
--- src/part_1_readable_bad/Project.java
+++ src/part_1_readable_bad/Project.java
@@ -10,50 +10,22 @@ package part_1_readable_bad;
 import java.util.List;
 
 public class Project {
-    String currency = "usd"; // currency
+    String c = "usd"; // currency
     // inventory
-    List<Item> inventory = List.of(
-            new Item(0, "Flashlight", "🔦", "A really great flashlight", 100, "usd"),
-            new Item(1, "Tin can", "🥫", "Pretty much what you would expect from a tin can", 32, "usd"),
-            new Item(2, "Cardboard Box", "📦", "It holds things", 5, "usd")
-    );
+    List<Inv> inv = List.of(new Inv("Flashlight", "🔦", "A really great flashlight", 100, 1, "usd"),
+            new Inv("Tin can", "🥫", "Pretty much what you would expect from a tin can", 32, 2, "usd"),
+            new Inv("Cardboard Box", "📦", "It holds things", 5, 3, "usd"));
 
     public Project() {
-        System.out.println("1 Readable Bad ❌ (fixed in class 😃)");
-        System.out.println("Global currency: " + currency);
-
-        generateTable();
-    }
-
-    private void generateTable() {
-        String border = "+-----------------+-------+----------------------------------------------------+-------+%n";
-        String header = "| Product         | Image | Description                                        | Price |%n";
-
-        System.out.format(border);
-        System.out.format(header);
-        System.out.format(border);
-        generateRows(inventory);
-        System.out.format(border);
-    }
-
-    private void generateRows(List<Item> inventory) {
+        System.out.println("1 Readable Bad ❌");
+        System.out.println("Global currency: " + c);
         String format = "| %-15s | %-5s | %-50s | %-5d |%n";
-
-        for (Item item : inventory) {
-            String product = item.product();
-            String image = item.image();
-            String description = item.description();
-            int price = item.price();
-
-            System.out.format(format, product, image, description, price);
+        System.out.format("+-----------------+-------+----------------------------------------------------+-------+%n");
+        System.out.format("| Product         | Image | Description                                        | Price |%n");
+        System.out.format("+-----------------+-------+----------------------------------------------------+-------+%n");
+        for (int i = 0; i < inv.size(); i++) {
+            System.out.format(format, inv.get(i).product(), inv.get(i).img(), inv.get(i).desc(), inv.get(i).price());
         }
+        System.out.format("+-----------------+-------+----------------------------------------------------+-------+%n");
     }
 }
-
-
-
-
-
-
-
-
